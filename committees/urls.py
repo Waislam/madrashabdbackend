1428365167887ecc0c26e4b201f0e8d0cbd49c9d
Waislam <waislam67@gmail.com>
2022-12-21 19:17:58 +0600

    extra

--- committees/urls.py
+++ committees/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('permanent-members/details/<int:pk>/', views.PermanentMembersDetail.as_view()),
     path('permanent-members/<phone_number>/details/', views.PermanentMembersDetailsWithPhoneFilter.as_view()),
 
+
     path('<madrasha_slug>/other-members/', views.OtherMembersListView.as_view()),
     path('other-member/details/<int:pk>/', views.OtherMembersDetail.as_view()),
 ]
