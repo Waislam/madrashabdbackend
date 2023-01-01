8f1759992679fbbbcaeed16a3b98625b01f365b8
Waislam <waislam67@gmail.com>
2023-01-02 01:05:50 +0600

    extra

--- students/urls.py
+++ students/urls.py
@@ -9,8 +9,8 @@ from .views import (
 
 urlpatterns = [
     path('<int:madrasha_slug>/', StudentView.as_view()),
-    path('detail/<slug:slug>/', StudentDetailView.as_view()),
-    path('detail/old-student/<slug:slug>/', OldStudentUpdateView.as_view()),
+    path('detail/<madrasha_slug>/<slug:slug>/', StudentDetailView.as_view()),
+    path('detail/old-student/<madrasha_slug>/<slug:slug>/', OldStudentUpdateView.as_view()),
     path('detail-by-id/<str:student_id>/<madrasha_slug>/', StudentDetailBySlugView.as_view()),
     path('check-passport/<str:passport_number>/', CheckUniquePassportNumber.as_view()),
     path('check-nid/<str:nid_number>/', CheckUniqueNIDNumber.as_view()),
