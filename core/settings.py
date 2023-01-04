8fb48a6251ca1c6419156633cc56c49e6467a9ff
niloy <niloy369@gmail.com>
2023-01-04 09:27:30 +0600

    fix: change config 11

--- core/settings.py
+++ core/settings.py
@@ -201,7 +201,7 @@ CSRF_TRUSTED_ORIGINS = [
         "http://178.128.94.215",
         "http://ec2-15-206-185-206.ap-south-1.compute.amazonaws.com:1337",
         "https://dev.ikhwanbd.com",
-        "http://devadmin.ikhwanbd.com:1337"
+        "https://devadmin.ikhwanbd.com"
     ]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
